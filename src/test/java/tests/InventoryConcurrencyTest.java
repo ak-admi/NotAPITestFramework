96473656d9d5882dca5fdfb4b1fca4c648891768
ak-admi <hangarialok@gmail.com>
2026-03-12 11:28:02 +0530

    concurrentPurchase for Optimistic Locking

--- src/test/java/tests/InventoryConcurrencyTest.java
+++ src/test/java/tests/InventoryConcurrencyTest.java
@@ -12,6 +12,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.is;
@@ -52,5 +53,53 @@ public class InventoryConcurrencyTest extends BaseTest {
         System.out.println(InitialStock+" was initial stock, now Remaining stocks: "+stock);
 
         Assert.assertTrue(stock>=0);
+    }
+
+    @Test
+    public void testConcurrentPurchaseWithOptimisticLock() throws InterruptedException{
+        int numberOfUsers =10;
+
+        AtomicInteger successCount = new AtomicInteger(0);
+        AtomicInteger conflictCount = new AtomicInteger(0);
+        AtomicInteger otherFailures = new AtomicInteger(0);
+
+        ExecutorService executor= Executors.newFixedThreadPool(numberOfUsers);
+        CountDownLatch latch = new CountDownLatch(1);
+        for(int i=0;i<numberOfUsers;i++){
+            executor.submit(()->{
+                try {
+                    latch.await();
+
+                int statusCode=RestAssured.given()
+                        .queryParam("quantity",2)
+                        .post("/inventory/7/decrement")
+                        .then()
+                        .extract()
+                        .statusCode();
+                if (statusCode == 200) {
+                    successCount.incrementAndGet();
+                } else if (statusCode == 409) {
+                    conflictCount.incrementAndGet();
+                } else {
+                    otherFailures.incrementAndGet();
+                    System.out.println("Unexpected status: " + statusCode);
+                }
+                }catch (InterruptedException e){
+                    throw new RuntimeException(e);
+                }
+
+            });
+        }
+        latch.countDown();
+
+        executor.shutdown();
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+        System.out.println("========== RESULTS ==========");
+        System.out.println("Total Users:    " + numberOfUsers);
+        System.out.println("200 OK:           " + successCount.get());
+        System.out.println("409 Conflict:     " + conflictCount.get());
+        System.out.println("Other failures:   " + otherFailures.get());
+        System.out.println("============================");
+
     }
 }
